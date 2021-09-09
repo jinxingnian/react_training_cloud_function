011f105e77ca701ec5546e31f26365a6ad8b5bd8
peter <peter_jin@worldvision.ca>
2021-09-09 14:58:39 -0400

    Completed cloud functions.

---
+++ functions/firebaseinit.js
@@ -0,0 +1,16 @@
+// const { initializeApp } = require("firebase-admin");
+// const { getFirestore } = require("firebase-admin");
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAiO4zPcYdvMU2JqdeLjmCuie8_E4qCzCU",
+//     authDomain: "react-training-a365d.firebaseapp.com",
+//     projectId: "react-training-a365d",
+//     storageBucket: "react-training-a365d.appspot.com",
+//     messagingSenderId: "732185122970",
+//     appId: "1:732185122970:web:87789745c3160c47672723",
+//     measurementId: "G-25PED5YB6M"
+//   };
+
+// const app = initializeApp(firebaseConfig);
+
+// // export const db = getFirestore();
--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,30 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+const cors = require('cors')({origin: true});
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const db = admin.firestore();
+
+
+
+exports.getColor = functions.https.onRequest((req, res) => {
+    // .../p1
+    cors(req, res, () => {
+        var player = req.url.substring(1);
+        admin.firestore().collection('playerColor').doc(player).get().then(doc => {
+            res.send(doc.data().color);
+        });
+    });
+});
+
+exports.setColor = functions.https.onRequest((req, res) => {
+    // .../p1 {color: 'red'}
+    cors(req, res, () => {
+        admin.firestore().collection('playerColor').doc(req.url.substring(1)).update({
+            "color": req.body.color
+        });
+        res.send("Update success!");
+    });
+    
+});
